0891aa95bea5b638240f71ac2d47e881a9865e97
Ryan Schroeder <ryan@schroeder247.com>
2025-07-26 13:36:22 -0700

    Fix exec() function namespace issue in CheckMonitor job

--- app/Jobs/CheckMonitor.php
+++ app/Jobs/CheckMonitor.php
@@ -166,13 +166,13 @@ class CheckMonitor implements ShouldQueue
         
         // Detect OS and use appropriate ping command
         if (PHP_OS_FAMILY === 'Windows') {
-            $pingCommand = "ping -n 1 -w " . ($this->monitor->timeout * 1000) . " " . escapeshellarg($host);
+            $pingCommand = "ping -n 1 -w " . ($this->monitor->timeout * 1000) . " " . \escapeshellarg($host);
         } else {
             // Linux/Unix ping command
-            $pingCommand = "ping -c 1 -W " . $this->monitor->timeout . " " . escapeshellarg($host);
+            $pingCommand = "ping -c 1 -W " . $this->monitor->timeout . " " . \escapeshellarg($host);
         }
         
-        $pingResult = exec($pingCommand, $output, $returnCode);
+        $pingResult = \exec($pingCommand, $output, $returnCode);
         
         $endTime = microtime(true);
         $result['response_time'] = round(($endTime - $startTime) * 1000);
